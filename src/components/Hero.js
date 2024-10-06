99e96fba644cf65d487c164054bd06ae69927d91
Luciano Massa <lgmassa98@gmail.com>
2024-10-05 23:25:56 -0300

    fixed ESlint #2

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,12 +5,14 @@ const Hero = () => {
   return (
     <section className="hero-container">
       <iframe
+        title="Instagram Reel"  // El atributo "title" es obligatorio para accesibilidad
         className="hero-iframe"
         src="https://www.instagram.com/reel/C_6sMuCpjjg/embed/captioned/"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
-      ></iframe>
+        
+      />
     </section>
   );
 };
