a1ae58ee6d50f6e88df9b20c6915dd0cf213d4fc
Luciano Massa <lgmassa98@gmail.com>
2024-10-05 23:08:47 -0300

    fixed ESlint

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import './Footer.css';
 import {
-  RiCheckboxBlankCircleFill,
   RiInstagramLine,
   RiFacebookLine,
-  RiTwitterLine,
   RiGithubLine,
   RiLinkedinFill 
   
