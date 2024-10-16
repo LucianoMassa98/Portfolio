104bdcfa9bf7d345c80a0afbc38caad51b78a7d7
Luciano Massa <lgmassa98@gmail.com>
2024-10-16 14:10:38 -0300

    add link works

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import CTA from './components/CTA';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Slide from './components/Slide'
+import DownloadPDF from './components/DownloadPDF'
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <Slide />
       <Skills />
       <CTA />
+      <DownloadPDF />
       <Footer />
     </div>
   );
